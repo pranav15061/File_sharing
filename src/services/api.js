166f2b1d15c8f3f586971484230fa2640aa5b6ae
Pranav <pranavbagal4@gmail.com>
2024-06-22 15:52:12 +0530

    final commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_URI = 'http://localhost:8000';
+// const API_URI = 'http://localhost:8000';
+
+const API_URI = 'https://file-b.onrender.com'
+// https://file-b.onrender.com
 
 export const uploadFile = async (data) => {
     try {
@@ -10,3 +13,4 @@ export const uploadFile = async (data) => {
         console.log('Error while calling the API ', error.message);
     }
 }
+
